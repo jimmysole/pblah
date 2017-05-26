90a581239362d6407c97ac40a50372ccdf1e8200
Jimmy Sole <jimmysole@gmail.com>
2017-05-26 18:28:50 -0400

    format

--- pblah/module/Members/src/Members/Model/GroupsModel.php
+++ pblah/module/Members/src/Members/Model/GroupsModel.php
@@ -3,15 +3,11 @@ namespace Members\Model;
 
 use Members\Model\Classes\Groups;
 use Members\Model\Classes\GroupMembersOnline;
-
 use Zend\Db\TableGateway\TableGateway;
 use Zend\Db\Sql\Select;
 use Zend\Db\Adapter\Adapter;
-
 use Members\Model\Filters\CreateGroup;
 use Members\Model\Filters\JoinGroup;
-
-
 
 class GroupsModel extends Groups
 {
@@ -22,35 +18,34 @@ class GroupsModel extends Groups
      */
     public $gateway;
 
-
     /**
      * Constructor method for GroupsModel class
      *
-     * @param TableGateway $gateway
-     * @param string $group_user
+     * @param TableGateway $gateway            
+     * @param string $group_user            
      */
     public function __construct(TableGateway $gateway, $group_user)
     {
         $this->gateway = $gateway instanceof TableGateway ? $gateway : null;
-
+        
         parent::getTableGateway($this->gateway);
         parent::getSQLClass();
         parent::setUser($group_user);
     }
-    
-    
+
     /**
      * Grabs the current user's id
+     * 
      * @return number
      */
     public function grabUserId()
     {
         return parent::getCurrentUserId();
     }
-    
-    
+
     /**
      * Gets the list of all the groups
+     * 
      * @return string[]
      */
     public function listAllGroups()
@@ -60,13 +55,13 @@ class GroupsModel extends Groups
 
     /**
      * Gets the list of groups joined by the user for displaying on the group home page
+     * 
      * @return array
      */
     public function listGroupsIndex()
     {
         return parent::getGroupsIndex();
     }
-
 
     /**
      * Gets the list of groups joined for the user
@@ -78,20 +73,20 @@ class GroupsModel extends Groups
         return parent::getGroups();
     }
 
-
     /**
      * Lets the user leave a group
-     * @param int $group_id
+     * 
+     * @param int $group_id            
      * @return boolean|array
      */
     public function leaveTheGroup($group_id)
     {
         return parent::leaveGroup($group_id);
     }
-    
-    
+
     /**
      * Lists all the groups for the user
+     * 
      * @return array
      */
     public function getAllUserGroups()
@@ -99,81 +94,73 @@ class GroupsModel extends Groups
         return parent::getMoreGroups();
     }
 
-    
     /**
      * Gets the member id for each group member
+     * 
      * @return string[]
      */
     public function partGroup()
     {
         return parent::getMemberGroups();
     }
-    
 
     /**
      * Gets all the group information
-     * @param int $group_id
+     * 
+     * @param int $group_id            
      * @return boolean|string[]|ArrayObject[]|NULL[]
      */
     public function getGroupInformation($group_id)
     {
-
+        
         // get the group admins
         $select_admins = new Select();
-
-
+        
         $select_admins->from(array(
-            'ga' => 'group_admins',
-        ))
-        ->join(array(
-            'm' => 'members'),
-            'ga.user_id = m.id', array('username'))
-        ->join(array(
-            'g' => 'groups',
-        ),  'ga.group_id = g.id')
-        ->where('g.id = ' . $group_id);
-
-
-        $query_group_admin = parent::$sql->getAdapter()
-        ->query(parent::$sql->buildSqlString($select_admins),
+            'ga' => 'group_admins'
+        ))->join(array(
+            'm' => 'members'
+        ), 'ga.user_id = m.id', array(
+            'username'
+        ))->join(array(
+            'g' => 'groups'
+        ), 'ga.group_id = g.id')
+            ->where('g.id = ' . $group_id);
+        
+        $query_group_admin = parent::$sql->getAdapter()->query(
+            parent::$sql->buildSqlString($select_admins), 
             Adapter::QUERY_MODE_EXECUTE);
-
+        
         $group_admins = array();
-
+        
         if (count($query_group_admin) == 0) {
             $group_admins[] = 'No admins found.';
         }
-
+        
         foreach ($query_group_admin as $group_admin) {
             $group_admins[] = $group_admin['username'];
         }
-
-
-
+        
         // get the group members
         $select = new Select();
-
-
+        
         $select->from(array(
             'g' => 'group_members'
-        ))
-        ->join(array(
+        ))->join(array(
             'm' => 'members'
-        ),  'g.member_id = m.id', array('username'))
-        ->join(array(
+        ), 'g.member_id = m.id', array(
+            'username'
+        ))->join(array(
             'grp' => 'groups'
-        ),  'g.group_id = grp.id')
+        ), 'g.group_id = grp.id')
         ->where(array(
             'g.group_id' => $group_id
         ));
-
-
-        $query = parent::$sql->getAdapter()
-        ->query(parent::$sql->buildSqlString($select),
-            Adapter::QUERY_MODE_EXECUTE);
-
+        
+        $query = parent::$sql->getAdapter()->query(parent::$sql->buildSqlString($select), Adapter::QUERY_MODE_EXECUTE);
+        
         $member_username = array();
-
+        
         if (count($query) == 0) {
             $member_username[] = 'No members found.';
         }
@@ -181,52 +168,51 @@ class GroupsModel extends Groups
         foreach ($query as $member) {
             $member_username[] = $member['username'];
         }
-
-
+        
         // get the rest of the group info
         $fetch = $this->gateway->select(array(
             'id' => $group_id
         ));
-
+        
         $row = $fetch->current();
-
+        
         if (!$row) {
             return false;
         }
-
+        
         return array(
-            'admins'  => implode(", ", $group_admins),
+            'admins' => implode(", ", $group_admins),
             'members' => implode(", ", $member_username),
             'info' => $row
         );
     }
-    
-    
+
     /**
      * Allows user to join group
-     * @param int $group_id
-     * @param JoinGroup $data
+     * 
+     * @param int $group_id            
+     * @param JoinGroup $data            
      * @return boolean
      */
     public function joinTheGroup($group_id, JoinGroup $data)
     {
         return parent::joinGroup($group_id, $data);
     }
-    
 
     /**
      * Gets group members currently online
+     * 
      * @return array[][]
      */
     public function getGroupMemsOnline($group_id = null)
     {
         return GroupMembersOnline::getGroupMembersOnline($group_id);
     }
-    
-    
+
     /**
      * Creates a new group
-     * @param CreateGroup $group
+     * 
+     * @param CreateGroup $group            
      * @return boolean
      */
     public function createNewGroup(CreateGroup $group)
