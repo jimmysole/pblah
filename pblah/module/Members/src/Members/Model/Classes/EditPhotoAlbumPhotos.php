3a209410ee6d92a376a99ace62271f0d5e7e7453
Jimmy Sole <jimmysole@gmail.com>
2017-06-19 15:29:58 -0400

    synch to latest code base

--- pblah/module/Members/src/Members/Model/Classes/EditPhotoAlbumPhotos.php
+++ pblah/module/Members/src/Members/Model/Classes/EditPhotoAlbumPhotos.php
@@ -7,5 +7,43 @@ use Members\Model\Classes\Exceptions\PhotoAlbumException;
 
 class EditPhotoAlbumPhotos
 {
+    const EDIT_PHOTO_ALBUM_SUCCESS = 'Your photo album was successfully edited.';
     
+    const EDIT_PHOTO_ALBUM_FAILURE = 'Error editing your photo album.';
+    
+    
+    /**
+     * @var mixed
+     */
+    public $album_name;
+    
+    
+    /**
+     * @var array
+     */
+    public $edits = array();
+    
+    
+    /**
+     * Constructor
+     * @param mixed $album_name
+     * @param array $edits
+     * @throws PhotoAlbumException
+     */
+    public function __construct($album_name, array $edits)
+    {
+        try {
+            if (!empty($album_name) && count($edits, 1) > 0) {
+                $this->album_name = $album_name;
+                
+                foreach ($edits as $key => $value) {
+                    $this->edits[$key] = $value;
+                }
+            } else {
+                throw new PhotoAlbumException("Invalid photo album and/or edits provided, please fix this and try again.");
+            }
+        } catch (PhotoAlbumException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
--- pblah/module/Members/src/Members/Model/Classes/RemovePhotosPhotoAlbum.php
+++ pblah/module/Members/src/Members/Model/Classes/RemovePhotosPhotoAlbum.php
@@ -7,5 +7,43 @@ use Members\Model\Classes\Exceptions\PhotoAlbumException;
 
 class RemovePhotosPhotoAlbum
 {
+    const REMOVE_PHOTO_ALBUM_SUCCESS = 'Photos successfully removed from your photo album.';
     
+    const REMOVE_PHOTO_ALBUM_FAILURE = 'Error removing photos from your photo album.';
+    
+    
+    /**
+     * @var mixed
+     */
+    public $album_name;
+    
+    
+    /**
+     * @var array
+     */
+    public $photos = array();
+    
+    
+    /**
+     * Constructor
+     * @param mixed $album_name
+     * @param array $photos
+     * @throws PhotoAlbumException
+     */
+    public function __construct($album_name, array $photos)
+    {
+        try {
+            if (!empty($album_name) && count($photos, 1) > 0) {
+                $this->album_name = $album_name;
+                
+                foreach ($photos as $key => $value) {
+                    $this->photos[$key] = $value;
+                }
+            } else {
+                throw new PhotoAlbumException("Invalid photo album and/or photos provided, please fix this and try again.");
+            }
+        } catch (PhotoAlbumException $e) {
+            echo $e->getMessage();
+        }
+    }
 }
