3f801471b7963791c799297e39258ef6b792fe6c
Jimmy Sole <jimmysole@gmail.com>
2017-12-26 23:43:38 -0500

    begin work on friends + messaging classes.

--- pblah/module/Members/src/Members/Model/Classes/Friends.php
+++ pblah/module/Members/src/Members/Model/Classes/Friends.php
@@ -2,8 +2,95 @@
 
 namespace Members\Model\Classes;
 
+use Members\Model\Classes\Exceptions\FriendsException;
+
+use Zend\Db\Sql\Select;
+use Zend\Db\Adapter\Adapter;
+
 
 class Friends extends Members
 {
+    /**
+     * @var string
+     */
+    protected $user;
     
+    /**
+     * @var array
+     */
+    protected $browse_results = array();
+    
+    
+    public function __construct()
+    {
+        $this->user = parent::getUser();
+    }
+    
+    
+    public function browseFriends($critera = null, array $critera_params = array())
+    {
+        if (null !== $critera) {
+            $select = new Select('profiles');
+            
+            // determine what critera was passed
+            if ($critera == 'age') {
+                $select->columns(array('profle_id', 'display_name', 'age', 'location', 'bio'))
+                ->where(array('age' => intval($critera['age'])));
+                
+                $query = parent::getSQLClass()->getAdapter()->query(
+                    parent::getSQLClass()->buildSqlString($select),
+                    Adapter::QUERY_MODE_EXECUTE
+                );
+                
+                if (count($query) > 0) {
+                    foreach ($query as $key => $value) {
+                        $this->browse_results[$key] = $value;
+                    }
+                    
+                    return $this->browse_results;
+                } else {
+                    throw new FriendsException("No friends were found with " . $critera['age'] . " as the critera.");
+                }
+                
+            } else if ($critera == 'display_name') {
+                
+            } else if ($critera == 'location') {
+                
+            } else {
+                throw new FriendsException("Invalid search critera passed.");
+            }
+        } else {
+            
+        }
+    }
+    
+    
+    public function sendAddRequest($friend_name, array $params = array())
+    {
+        
+    }
+    
+    
+    public function cancelAddRequest()
+    {
+        
+    }
+    
+    
+    public function blockFriendRequest($who, array $params = array())
+    {
+        
+    }
+    
+    
+    public function approveFriendRequest()
+    {
+        
+    }
+    
+    
+    public function denyFriendRequest()
+    {
+        
+    }
 }
--- pblah/module/Members/src/Members/Model/Classes/Messages.php
+++ pblah/module/Members/src/Members/Model/Classes/Messages.php
@@ -2,8 +2,35 @@
 
 namespace Members\Model\Classes;
 
+use Members\Model\Classes\Exceptions\MessagesException;
 
-class Messages
+
+class Messages extends Friends
 {
-
+    public $to;
+    
+    public $message = array();
+    
+    
+    public function __construct()
+    {
+       
+    }
+    
+    
+    public function sendMessage($to, array $message)
+    {
+        $this->to = (!empty($to)) ? $this->to = $to : null;
+        
+        if (count($message) > 0) {
+            foreach ($message as $key => $value) {
+                $this->message[$key] = $value;
+            }
+        } else {
+            throw new MessagesException("Cannot send a empty message.");
+        }
+        
+        // send the message to the user from the sender
+        
+    }
 }
