a79c2efff997c32d6c56cc69c5beeaa10144410e
Jimmy Sole <jimmysole@gmail.com>
2018-01-16 20:08:22 -0500

    add friend request service plus model

---
+++ pblah/module/Members/src/Members/Model/FriendRequestModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Members\Model;
+
+use Members\Model\Classes\Friends;
+use Zend\Db\TableGateway\TableGateway;
+
+
+class FriendRequestModel extends Friends
+{
+    public $gateway;
+    
+    
+    public function __construct(TableGateway $gateway, $user)
+    {
+        $this->gateway = $gateway instanceof TableGateway ? $gateway : null;
+        
+        parent::getTableGateway($this->gateway);
+        parent::setUser($user);
+    }
+}
